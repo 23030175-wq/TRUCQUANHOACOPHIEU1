bf1663bdfb2127f1638f6f0fe43752894e6f3fbe
23030175-wq <23030175@student.bdu.edu.vn>
2026-06-28 16:00:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.image("logo.jpg")
 # TIÊU ĐỀ
 # =============================
 st.title("📈 TRỰC QUAN HÓA GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-st.subheader("TS. VŨ ĐỨC BÌNH")
+st.subheader("SV.TRẦN CÔNG TUYỀN")
 
 st.markdown("---")
 
